6ffdf1deef6de2edf1686c93b32884798fc680e9
AdrianD <Adrian.Domagala71@gmail.com>
2025-09-05 22:02:04 +0200

    da

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         404: resolve(__dirname, '404.html'),
         esseys: resolve(__dirname, 'esseys.html'),
+        essey1: resolve(__dirname, 'przekminki/czy_sa_szanse.html'),
         projektDashboard: resolve(__dirname, 'projekt-dashboard.html')
       }
     }
